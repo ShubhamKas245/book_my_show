7b77c70389d38df4b4afa13932ea84dcad3a2373
ShubhamKas245 <ShubhamKas245@gmail.com>
2023-05-20 17:10:50 +0530

    basic setup completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// React Slick
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center font-bold my-3 text-2xl">
+        Welcome to Book_My_Show
+      </h1>
+    </div>
+  );
+}
+
+export default App;
